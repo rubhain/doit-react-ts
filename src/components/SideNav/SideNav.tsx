ea46f873de1da60884cb1b9cbab62342c39a977e
Bugrahan <bugrahantufekcioglu@gmail.com>
2023-09-07 09:24:28 +0300

    Boards list

--- src/components/SideNav/SideNav.tsx
+++ src/components/SideNav/SideNav.tsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
+import { useGetBoardsQuery } from '../../storage/apiSlice';
+import { IBoard } from '../../storage/types';
 
 function SideNav() {
     const [sideBar, setSideBar] = useState(true);
 
+    const { data, isLoading } = useGetBoardsQuery('');
+
+    console.log(data, isLoading);
+
+
     const showSideBar = () => {
         setSideBar(!sideBar)
     }
-
+    if (isLoading === true) {
+        return <div className='navbar'>Loading...</div>
+    }
     return (
         <div className='navbar'>
             <a onClick={showSideBar}>DOIT</a>
             <nav className={sideBar ? "nav-menu active" : "nav-menu"}>
                 <ul className='nav-menu-items' >
+                    {data.map((item: IBoard) => <li key={item.id} className='nav-text'>{item.title}</li>)}
                     <li className='nav-text' >
                         <a style={{ borderStyle: 'solid' }} >Add New Board</a>
                     </li>
